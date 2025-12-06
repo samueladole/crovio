d3292006a65e9fbffcd9be10e847e9f51e43f705
samueladole <adoleichasamuel@gmail.com>
2025-12-06 05:21:20 +0000

    Update alembic migrations

--- backend/app/db/base.py
+++ backend/app/db/base.py
@@ -1,2 +1,4 @@
-from models.dealer import Dealer
-from db.session import Base
+"""Import all the models, so that Base has them before being imported by Alembic."""
+from app.db.session import Base
+from app.models.dealer import Dealer
+from app.models.users import User
